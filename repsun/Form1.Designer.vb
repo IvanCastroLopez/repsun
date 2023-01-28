499e2dee59f501d4d05e9e81addc7086f60fe86b
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-01-28 21:36:14 +0100

    Prueba para aprender a usar git.

--- repsun/Form1.Designer.vb
+++ repsun/Form1.Designer.vb
@@ -23,23 +23,34 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 42.0!)
-        Me.Label1.Location = New System.Drawing.Point(256, 145)
+        Me.Label1.Location = New System.Drawing.Point(249, 62)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(276, 79)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Repsun"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(315, 247)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(210, 95)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -49,4 +60,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
