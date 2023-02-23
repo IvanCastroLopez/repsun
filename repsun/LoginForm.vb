671dafc7ba72df5f61ebea4633ac44a9a5f48291
Leo Aguado <35865347+leoaguado@users.noreply.github.com>
2023-02-23 22:15:53 +0100

    Cambios login

--- repsun/LoginForm.vb
+++ repsun/LoginForm.vb
@@ -3,6 +3,7 @@
 Public Class LoginForm
     Public conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Repsol_db.accdb")
     Public adaptador As New OleDbDataAdapter("Select * from Usuarios", conexion)
+    Public midataset As New DataSet
     Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Cambiar tamaño de estos
         ' lbl_bienvenido
@@ -20,11 +21,17 @@ Public Class LoginForm
     ''' <param name="sender">Botón inicio sesión</param>
     ''' <param name="e">Click</param>
     Private Sub btn_inicioSesion_Click(sender As Object, e As EventArgs) Handles btn_inicioSesion.Click
+        adaptador.Fill(midataset, "Usuarios")
+        Dim usuario As String
+        Dim contrasena As String
         If Validaciones.ValidarNombreUsuario(txt_user.Text) And Validaciones.ValidarContrasena(txt_contrasena.Text) Then
 
 
             MenuPrincipalForm.Show()
             Me.Hide()
+        Else
+            Errores.grabarError("Acceso Inválido", "Acceso invalido")
+
         End If
     End Sub
 
