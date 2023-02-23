66b4d53eece1cb243ec4c7fcd0c07ffe0bcdc1c7
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-02-23 22:48:23 +0100

    Delete UtilidadResolucion.vb

--- repsun/LoginForm.vb
+++ repsun/LoginForm.vb
@@ -4,16 +4,6 @@ Public Class LoginForm
     Public conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Repsol_db.accdb")
     Public adaptador As New OleDbDataAdapter("Select * from Usuarios", conexion)
     Public midataset As New DataSet
-    Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Cambiar tamaño de estos
-        ' lbl_bienvenido
-        ' lbl_username
-        ' txt_user
-        ' lbl_contrasena
-        ' txt_contrasena
-        ' btn_inicioSesion
-
-    End Sub
 
     ''' <summary>
     ''' Abre el formulario "MenuPrincipalForm" y oculta el formulario actual.
