314d10434ba45dca96f37bd513f9b7f7f2bd6d03
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-02-24 00:52:00 +0100

    GestiónForm

--- repsun/GestionForm.Designer.vb
+++ repsun/GestionForm.Designer.vb
@@ -23,24 +23,103 @@ Partial Class GestionForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.tbc_gestion = New System.Windows.Forms.TabControl()
+        Me.tpg_combustible = New System.Windows.Forms.TabPage()
+        Me.tpg_tienda = New System.Windows.Forms.TabPage()
+        Me.tpg_empleados = New System.Windows.Forms.TabPage()
+        Me.tpg_proveedores = New System.Windows.Forms.TabPage()
+        Me.tpg_clientes = New System.Windows.Forms.TabPage()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.tbc_gestion.SuspendLayout()
         Me.SuspendLayout()
         '
         'TableLayoutPanel1
         '
         Me.TableLayoutPanel1.ColumnCount = 3
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.30903!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.69097!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 65.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.15!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 93.15!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.7!))
+        Me.TableLayoutPanel1.Controls.Add(Me.tbc_gestion, 1, 3)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 4
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.18182!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.81818!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 872.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.05!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.05!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.05!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.85!))
         Me.TableLayoutPanel1.Size = New System.Drawing.Size(1904, 1041)
         Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'tbc_gestion
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.tbc_gestion, 2)
+        Me.tbc_gestion.Controls.Add(Me.tpg_combustible)
+        Me.tbc_gestion.Controls.Add(Me.tpg_tienda)
+        Me.tbc_gestion.Controls.Add(Me.tpg_empleados)
+        Me.tbc_gestion.Controls.Add(Me.tpg_proveedores)
+        Me.tbc_gestion.Controls.Add(Me.tpg_clientes)
+        Me.tbc_gestion.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tbc_gestion.Font = New System.Drawing.Font("Yu Gothic UI", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbc_gestion.Location = New System.Drawing.Point(552, 295)
+        Me.tbc_gestion.Margin = New System.Windows.Forms.Padding(2)
+        Me.tbc_gestion.Name = "tbc_gestion"
+        Me.TableLayoutPanel1.SetRowSpan(Me.tbc_gestion, 2)
+        Me.tbc_gestion.SelectedIndex = 0
+        Me.tbc_gestion.Size = New System.Drawing.Size(800, 450)
+        Me.tbc_gestion.TabIndex = 2
+        '
+        'tpg_combustible
+        '
+        Me.tpg_combustible.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_combustible.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_combustible.Name = "tpg_combustible"
+        Me.tpg_combustible.Padding = New System.Windows.Forms.Padding(2)
+        Me.tpg_combustible.Size = New System.Drawing.Size(792, 412)
+        Me.tpg_combustible.TabIndex = 0
+        Me.tpg_combustible.Text = "Combustible"
+        Me.tpg_combustible.UseVisualStyleBackColor = True
+        '
+        'tpg_tienda
+        '
+        Me.tpg_tienda.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_tienda.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_tienda.Name = "tpg_tienda"
+        Me.tpg_tienda.Padding = New System.Windows.Forms.Padding(2)
+        Me.tpg_tienda.Size = New System.Drawing.Size(792, 412)
+        Me.tpg_tienda.TabIndex = 1
+        Me.tpg_tienda.Text = "Tienda"
+        Me.tpg_tienda.UseVisualStyleBackColor = True
+        '
+        'tpg_empleados
+        '
+        Me.tpg_empleados.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_empleados.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_empleados.Name = "tpg_empleados"
+        Me.tpg_empleados.Size = New System.Drawing.Size(792, 412)
+        Me.tpg_empleados.TabIndex = 2
+        Me.tpg_empleados.Text = "Empleados"
+        Me.tpg_empleados.UseVisualStyleBackColor = True
+        '
+        'tpg_proveedores
+        '
+        Me.tpg_proveedores.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_proveedores.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_proveedores.Name = "tpg_proveedores"
+        Me.tpg_proveedores.Size = New System.Drawing.Size(792, 412)
+        Me.tpg_proveedores.TabIndex = 3
+        Me.tpg_proveedores.Text = "Proveedores"
+        Me.tpg_proveedores.UseVisualStyleBackColor = True
+        '
+        'tpg_clientes
+        '
+        Me.tpg_clientes.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_clientes.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_clientes.Name = "tpg_clientes"
+        Me.tpg_clientes.Size = New System.Drawing.Size(792, 412)
+        Me.tpg_clientes.TabIndex = 4
+        Me.tpg_clientes.Text = "Clientes"
+        Me.tpg_clientes.UseVisualStyleBackColor = True
         '
         'GestionForm
         '
@@ -53,9 +132,17 @@ Partial Class GestionForm
         Me.Name = "GestionForm"
         Me.Text = "GestionForm"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.tbc_gestion.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents tbc_gestion As TabControl
+    Friend WithEvents tpg_combustible As TabPage
+    Friend WithEvents tpg_tienda As TabPage
+    Friend WithEvents tpg_empleados As TabPage
+    Friend WithEvents tpg_proveedores As TabPage
+    Friend WithEvents tpg_clientes As TabPage
 End Class
