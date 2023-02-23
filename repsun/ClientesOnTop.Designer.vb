12a86bc66c7c7007313941d18b9cdea18035bb8e
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-02-24 00:11:31 +0100

    Creación ClientesOnTop

---
+++ repsun/ClientesOnTop.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ClientesOnTop
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tbc_gestion = New System.Windows.Forms.TabControl()
+        Me.tpg_combustible = New System.Windows.Forms.TabPage()
+        Me.tpg_tienda = New System.Windows.Forms.TabPage()
+        Me.tpg_empleados = New System.Windows.Forms.TabPage()
+        Me.tpg_proveedores = New System.Windows.Forms.TabPage()
+        Me.tpg_clientes = New System.Windows.Forms.TabPage()
+        Me.tbc_gestion.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tbc_gestion
+        '
+        Me.tbc_gestion.Controls.Add(Me.tpg_combustible)
+        Me.tbc_gestion.Controls.Add(Me.tpg_tienda)
+        Me.tbc_gestion.Controls.Add(Me.tpg_empleados)
+        Me.tbc_gestion.Controls.Add(Me.tpg_proveedores)
+        Me.tbc_gestion.Controls.Add(Me.tpg_clientes)
+        Me.tbc_gestion.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tbc_gestion.Font = New System.Drawing.Font("Yu Gothic UI", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbc_gestion.Location = New System.Drawing.Point(0, 0)
+        Me.tbc_gestion.Margin = New System.Windows.Forms.Padding(2)
+        Me.tbc_gestion.Name = "tbc_gestion"
+        Me.tbc_gestion.SelectedIndex = 0
+        Me.tbc_gestion.Size = New System.Drawing.Size(800, 450)
+        Me.tbc_gestion.TabIndex = 1
+        '
+        'tpg_combustible
+        '
+        Me.tpg_combustible.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_combustible.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_combustible.Name = "tpg_combustible"
+        Me.tpg_combustible.Padding = New System.Windows.Forms.Padding(2)
+        Me.tpg_combustible.Size = New System.Drawing.Size(792, 412)
+        Me.tpg_combustible.TabIndex = 0
+        Me.tpg_combustible.Text = "Combustible"
+        Me.tpg_combustible.UseVisualStyleBackColor = True
+        '
+        'tpg_tienda
+        '
+        Me.tpg_tienda.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_tienda.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_tienda.Name = "tpg_tienda"
+        Me.tpg_tienda.Padding = New System.Windows.Forms.Padding(2)
+        Me.tpg_tienda.Size = New System.Drawing.Size(1769, 888)
+        Me.tpg_tienda.TabIndex = 1
+        Me.tpg_tienda.Text = "Tienda"
+        Me.tpg_tienda.UseVisualStyleBackColor = True
+        '
+        'tpg_empleados
+        '
+        Me.tpg_empleados.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_empleados.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_empleados.Name = "tpg_empleados"
+        Me.tpg_empleados.Size = New System.Drawing.Size(1769, 888)
+        Me.tpg_empleados.TabIndex = 2
+        Me.tpg_empleados.Text = "Empleados"
+        Me.tpg_empleados.UseVisualStyleBackColor = True
+        '
+        'tpg_proveedores
+        '
+        Me.tpg_proveedores.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_proveedores.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_proveedores.Name = "tpg_proveedores"
+        Me.tpg_proveedores.Size = New System.Drawing.Size(1769, 888)
+        Me.tpg_proveedores.TabIndex = 3
+        Me.tpg_proveedores.Text = "Proveedores"
+        Me.tpg_proveedores.UseVisualStyleBackColor = True
+        '
+        'tpg_clientes
+        '
+        Me.tpg_clientes.Location = New System.Drawing.Point(4, 34)
+        Me.tpg_clientes.Margin = New System.Windows.Forms.Padding(2)
+        Me.tpg_clientes.Name = "tpg_clientes"
+        Me.tpg_clientes.Size = New System.Drawing.Size(1769, 888)
+        Me.tpg_clientes.TabIndex = 4
+        Me.tpg_clientes.Text = "Clientes"
+        Me.tpg_clientes.UseVisualStyleBackColor = True
+        '
+        'ClientesOnTop
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.tbc_gestion)
+        Me.Name = "ClientesOnTop"
+        Me.Text = "ClientesOnTop"
+        Me.tbc_gestion.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tbc_gestion As TabControl
+    Friend WithEvents tpg_combustible As TabPage
+    Friend WithEvents tpg_tienda As TabPage
+    Friend WithEvents tpg_empleados As TabPage
+    Friend WithEvents tpg_proveedores As TabPage
+    Friend WithEvents tpg_clientes As TabPage
+End Class
