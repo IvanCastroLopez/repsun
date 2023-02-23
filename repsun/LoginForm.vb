ed350d9f4a284185a25b79dd983fbec38ef5d4f1
Leo Aguado <35865347+leoaguado@users.noreply.github.com>
2023-02-23 22:56:06 +0100

    cambios login

--- repsun/LoginForm.vb
+++ repsun/LoginForm.vb
@@ -1,9 +1,13 @@
 ﻿Imports System.Data.OleDb
+Imports System.Data.SqlClient
 
 Public Class LoginForm
     Public conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Repsol_db.accdb")
     Public adaptador As New OleDbDataAdapter("Select * from Usuarios", conexion)
     Public midataset As New DataSet
+
+    Dim query As String = "SELECT * FROM usuarios WHERE nombre_usuario = @usuario AND contrasena = @contrasena"
+    Dim comando As New OleDbCommand(query, conexion)
     Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Cambiar tamaño de estos
         ' lbl_bienvenido
@@ -22,18 +26,31 @@ Public Class LoginForm
     ''' <param name="e">Click</param>
     Private Sub btn_inicioSesion_Click(sender As Object, e As EventArgs) Handles btn_inicioSesion.Click
         adaptador.Fill(midataset, "Usuarios")
-        Dim usuario_inp As String
+        Dim usuario As String
         Dim contrasena As String
         If Validaciones.ValidarNombreUsuario(txt_user.Text) And Validaciones.ValidarContrasena(txt_contrasena.Text) Then
-            MenuPrincipalForm.Show()
-            Me.Hide()
+            comando.Parameters.AddWithValue("@usuario", txt_user.Text)
+            comando.Parameters.AddWithValue("@contrasena", txt_contrasena.Text)
+            conexion.Open()
+
+            Dim reader As OleDbDataReader = comando.ExecuteReader
+
+            If reader.Read() Then
+                Dim usuarioBD As String = reader("nombre_usuario").ToString()
+                Dim contraseñaBD As String = reader("contraseña").ToString()
+
+                MenuPrincipalForm.Show()
+                Me.Hide()
+            Else
+                MessageBox.Show("Usuario o contraseña incorrectos.")
+            End If
         Else
             Errores.grabarError("Acceso Inválido", "Acceso invalido")
-
         End If
     End Sub
 
     Private Sub pbx_exit_Click(sender As Object, e As EventArgs) Handles pbx_exit.Click
+        conexion.Close()
         Me.Close()
     End Sub
 End Class
