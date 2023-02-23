aa23c383298cae7287599bf23708ab861d027fa0
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-02-23 23:51:53 +0100

    Herramientas completas y comentadas

--- repsun/Utilidades/Herramientas.vb
+++ repsun/Utilidades/Herramientas.vb
@@ -10,33 +10,61 @@ Public Class Herramientas
     ''' <param name="tipo_combustible"></param>
     ''' <returns></returns>
     Public Shared Function cambioLitrosPrecio(litros As Decimal, tipo_combustible As tipoCombustible) As Decimal
+
+        ' Se crea una conexión a la base de datos de combustibles.
         Dim conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Repsol_db.accdb")
+        ' Se crea un adaptador para acceder a la tabla de combustibles.
         Dim adaptador As New OleDbDataAdapter("Select * from Combustible", conexion)
+        ' Se crea un dataset para almacenar los datos obtenidos de la tabla de contenidos.
         Dim midataset As New DataSet
 
+        ' Se crea un comando que selecciona los datos del combustible deseado.
         Dim comando As New OleDbCommand("select * from Combustible where tipo_combustible=@com", conexion)
         comando.Parameters.AddWithValue("@com", tipo_combustible.ToString)
 
+        ' Se abre la conexión a la base de datos.
         conexion.Open()
+
+        ' Se ejecuta el comando y se obtiene un data reader con los datos del combustible.
         Dim res As OleDbDataReader = comando.ExecuteReader
+
+        ' Se obtiene el precio por litro del combustible deseado.
         Dim precioPorLitro As Decimal = res("precio_por_litro")
 
+        ' Se cierra la conexión a la base de datos.
+        conexion.Close()
+
+        ' Devolver el precio de los litros introducidos.
         Return litros * precioPorLitro
     End Function
 
-    Public Shared Function cambioPrecioLitros(precio As Decimal, tipo_combustible As tipoCombustible) As Decimal
+    Public Shared Function cambioPrecioLitros(dinero As Decimal, tipo_combustible As tipoCombustible) As Decimal
+
+        ' Se crea una conexión a la base de datos de combustibles.
         Dim conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Repsol_db.accdb")
+        ' Se crea un adaptador para acceder a la tabla de combustibles.
         Dim adaptador As New OleDbDataAdapter("Select * from Combustible", conexion)
+        ' Se crea un dataset para almacenar los datos obtenidos de la tabla de contenidos.
         Dim midataset As New DataSet
 
+        ' Se crea un comando que selecciona los datos del combustible deseado.
         Dim comando As New OleDbCommand("select * from Combustible where tipo_combustible=@com", conexion)
         comando.Parameters.AddWithValue("@com", tipo_combustible.ToString)
 
+        ' Se abre la conexión a la base de datos.
         conexion.Open()
+
+        ' Se ejecuta el comando y se obtiene un data reader con los datos del combustible.
         Dim res As OleDbDataReader = comando.ExecuteReader
+
+        ' Se obtiene el precio por litro del combustible deseado.
         Dim precioPorLitro As Decimal = res("precio_por_litro")
 
-        Return precio / precioPorLitro
+        ' Se cierra la conexión a la base de datos.
+        conexion.Close()
+
+        ' Devolver el litro equivalente al dinero introducido.
+        Return dinero / precioPorLitro
     End Function
 
     ' Enumeración que define los diferentes tipos de combustibles
