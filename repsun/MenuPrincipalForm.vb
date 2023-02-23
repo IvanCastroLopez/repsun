e381ac043178a30d62e1832b44cf29cfc5305322
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-02-23 19:04:58 +0100

    Biblioteca de clases

--- repsun/MenuPrincipalForm.vb
+++ repsun/MenuPrincipalForm.vb
@@ -7,4 +7,9 @@
     Private Sub btn_salir_Click(sender As Object, e As EventArgs) Handles btn_salir.Click
 
     End Sub
+
+    Private Sub btn_gestion_Click(sender As Object, e As EventArgs) Handles btn_gestion.Click
+        GestionForm.Show()
+        Me.Hide()
+    End Sub
 End Class
