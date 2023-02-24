a7c2fa982180c3902d149da524391be87ad43a91
Leo Aguado <35865347+leoaguado@users.noreply.github.com>
2023-02-25 00:23:25 +0100

    Gestion form conexion db

--- repsun/GestionForm.vb
+++ repsun/GestionForm.vb
@@ -1,17 +1,44 @@
 ﻿Imports System.Data.OleDb
 
 Public Class GestionForm
-    Dim usuarioConectado As String
+    Public usuarioConectado As String = LoginForm.userConnected
+    Public conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Repsol_db.accdb")
+    Public adaptador_tienda As New OleDbDataAdapter("Select * from Producto", conexion)
+    Public adaptador_empleados As New OleDbDataAdapter("Select * from Empleado", conexion)
+    Public adaptador_proveedores As New OleDbDataAdapter("Select * from Proveedor", conexion)
+    Public adaptador_clientes As New OleDbDataAdapter("Select * from ClienteRepsol", conexion)
+    Public gestion_dataset As New DataSet
     Private Sub GestionForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        usuarioConectado = LoginForm.userConnected
-        Dim conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Repsol_db.accdb")
-        Dim adaptador As New OleDbDataAdapter("Select * from Usuarios", conexion)
+        adaptador_tienda.Fill(gestion_dataset, "Producto")
+        adaptador_empleados.Fill(gestion_dataset, "Empleado")
+        adaptador_proveedores.Fill(gestion_dataset, "Proveedor")
+        adaptador_clientes.Fill(gestion_dataset, "ClienteRepsol")
+
+        Me.dgv_tienda.Font = New Font("Arial", 10, FontStyle.Regular)
+        dgv_tienda.DataSource = gestion_dataset
+        dgv_tienda.DataMember = "Producto"
+
+        Me.dgv_empleados.Font = New Font("Arial", 10, FontStyle.Regular)
+        dgv_empleados.DataSource = gestion_dataset
+        dgv_empleados.DataMember = "Tabla"
+
+        Me.dgv_proveedores.Font = New Font("Arial", 10, FontStyle.Regular)
+        dgv_proveedores.DataSource = gestion_dataset
+        dgv_proveedores.DataMember = "Tabla"
+
+        Me.dgv_clientes.Font = New Font("Arial", 10, FontStyle.Regular)
+        dgv_clientes.DataSource = gestion_dataset
+        dgv_clientes.DataMember = "Tabla"
     End Sub
 
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) 
+
+
+
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
         MenuPrincipalForm.Show()
         Me.Hide()
     End Sub
-
 
 End Class
