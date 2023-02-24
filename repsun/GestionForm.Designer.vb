1aabca1a3dee5fd2321a81633110e36dc92136fd
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-02-24 22:13:03 +0100

    gestion form 2

--- repsun/GestionForm.Designer.vb
+++ repsun/GestionForm.Designer.vb
@@ -23,12 +23,6 @@ Partial Class GestionForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.tlp_gestion = New System.Windows.Forms.TableLayoutPanel()
-        Me.tbc_gestion = New System.Windows.Forms.TabControl()
-        Me.tpg_combustible = New System.Windows.Forms.TabPage()
-        Me.tpg_tienda = New System.Windows.Forms.TabPage()
-        Me.tpg_empleados = New System.Windows.Forms.TabPage()
-        Me.tpg_proveedores = New System.Windows.Forms.TabPage()
-        Me.tpg_clientes = New System.Windows.Forms.TabPage()
         Me.pbx_close = New System.Windows.Forms.PictureBox()
         Me.tlp_categorias = New System.Windows.Forms.TableLayoutPanel()
         Me.pbx_icon = New System.Windows.Forms.PictureBox()
@@ -44,7 +38,6 @@ Partial Class GestionForm
         Me.lbl_proveedores = New System.Windows.Forms.Label()
         Me.lbl_clientes = New System.Windows.Forms.Label()
         Me.tlp_gestion.SuspendLayout()
-        Me.tbc_gestion.SuspendLayout()
         CType(Me.pbx_close, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.tlp_categorias.SuspendLayout()
         CType(Me.pbx_icon, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -62,89 +55,18 @@ Partial Class GestionForm
         Me.tlp_gestion.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.995657!))
         Me.tlp_gestion.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.64193!))
         Me.tlp_gestion.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.362419!))
-        Me.tlp_gestion.Controls.Add(Me.tbc_gestion, 1, 1)
         Me.tlp_gestion.Controls.Add(Me.pbx_close, 2, 0)
         Me.tlp_gestion.Controls.Add(Me.tlp_categorias, 0, 0)
         Me.tlp_gestion.Dock = System.Windows.Forms.DockStyle.Fill
         Me.tlp_gestion.Location = New System.Drawing.Point(0, 0)
         Me.tlp_gestion.Name = "tlp_gestion"
-        Me.tlp_gestion.RowCount = 2
+        Me.tlp_gestion.RowCount = 3
         Me.tlp_gestion.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.425209!))
         Me.tlp_gestion.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1.539203!))
         Me.tlp_gestion.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.03559!))
         Me.tlp_gestion.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.tlp_gestion.Size = New System.Drawing.Size(1412, 882)
         Me.tlp_gestion.TabIndex = 0
-        '
-        'tbc_gestion
-        '
-        Me.tlp_gestion.SetColumnSpan(Me.tbc_gestion, 2)
-        Me.tbc_gestion.Controls.Add(Me.tpg_combustible)
-        Me.tbc_gestion.Controls.Add(Me.tpg_tienda)
-        Me.tbc_gestion.Controls.Add(Me.tpg_empleados)
-        Me.tbc_gestion.Controls.Add(Me.tpg_proveedores)
-        Me.tbc_gestion.Controls.Add(Me.tpg_clientes)
-        Me.tbc_gestion.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.tbc_gestion.Font = New System.Drawing.Font("Yu Gothic UI", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbc_gestion.Location = New System.Drawing.Point(143, 41)
-        Me.tbc_gestion.Margin = New System.Windows.Forms.Padding(2)
-        Me.tbc_gestion.Name = "tbc_gestion"
-        Me.tlp_gestion.SetRowSpan(Me.tbc_gestion, 2)
-        Me.tbc_gestion.SelectedIndex = 0
-        Me.tbc_gestion.Size = New System.Drawing.Size(1267, 839)
-        Me.tbc_gestion.TabIndex = 2
-        '
-        'tpg_combustible
-        '
-        Me.tpg_combustible.Location = New System.Drawing.Point(4, 34)
-        Me.tpg_combustible.Margin = New System.Windows.Forms.Padding(2)
-        Me.tpg_combustible.Name = "tpg_combustible"
-        Me.tpg_combustible.Padding = New System.Windows.Forms.Padding(2)
-        Me.tpg_combustible.Size = New System.Drawing.Size(1259, 801)
-        Me.tpg_combustible.TabIndex = 0
-        Me.tpg_combustible.Text = "Combustible"
-        Me.tpg_combustible.UseVisualStyleBackColor = True
-        '
-        'tpg_tienda
-        '
-        Me.tpg_tienda.Location = New System.Drawing.Point(4, 34)
-        Me.tpg_tienda.Margin = New System.Windows.Forms.Padding(2)
-        Me.tpg_tienda.Name = "tpg_tienda"
-        Me.tpg_tienda.Padding = New System.Windows.Forms.Padding(2)
-        Me.tpg_tienda.Size = New System.Drawing.Size(1259, 790)
-        Me.tpg_tienda.TabIndex = 1
-        Me.tpg_tienda.Text = "Tienda"
-        Me.tpg_tienda.UseVisualStyleBackColor = True
-        '
-        'tpg_empleados
-        '
-        Me.tpg_empleados.Location = New System.Drawing.Point(4, 34)
-        Me.tpg_empleados.Margin = New System.Windows.Forms.Padding(2)
-        Me.tpg_empleados.Name = "tpg_empleados"
-        Me.tpg_empleados.Size = New System.Drawing.Size(1259, 790)
-        Me.tpg_empleados.TabIndex = 2
-        Me.tpg_empleados.Text = "Empleados"
-        Me.tpg_empleados.UseVisualStyleBackColor = True
-        '
-        'tpg_proveedores
-        '
-        Me.tpg_proveedores.Location = New System.Drawing.Point(4, 34)
-        Me.tpg_proveedores.Margin = New System.Windows.Forms.Padding(2)
-        Me.tpg_proveedores.Name = "tpg_proveedores"
-        Me.tpg_proveedores.Size = New System.Drawing.Size(1259, 790)
-        Me.tpg_proveedores.TabIndex = 3
-        Me.tpg_proveedores.Text = "Proveedores"
-        Me.tpg_proveedores.UseVisualStyleBackColor = True
-        '
-        'tpg_clientes
-        '
-        Me.tpg_clientes.Location = New System.Drawing.Point(4, 34)
-        Me.tpg_clientes.Margin = New System.Windows.Forms.Padding(2)
-        Me.tpg_clientes.Name = "tpg_clientes"
-        Me.tpg_clientes.Size = New System.Drawing.Size(1259, 790)
-        Me.tpg_clientes.TabIndex = 4
-        Me.tpg_clientes.Text = "Clientes"
-        Me.tpg_clientes.UseVisualStyleBackColor = True
         '
         'pbx_close
         '
@@ -343,7 +265,6 @@ Partial Class GestionForm
         Me.Text = "GestionForm"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.tlp_gestion.ResumeLayout(False)
-        Me.tbc_gestion.ResumeLayout(False)
         CType(Me.pbx_close, System.ComponentModel.ISupportInitialize).EndInit()
         Me.tlp_categorias.ResumeLayout(False)
         Me.tlp_categorias.PerformLayout()
@@ -359,12 +280,6 @@ Partial Class GestionForm
     End Sub
 
     Friend WithEvents tlp_gestion As TableLayoutPanel
-    Friend WithEvents tbc_gestion As TabControl
-    Friend WithEvents tpg_combustible As TabPage
-    Friend WithEvents tpg_tienda As TabPage
-    Friend WithEvents tpg_empleados As TabPage
-    Friend WithEvents tpg_proveedores As TabPage
-    Friend WithEvents tpg_clientes As TabPage
     Friend WithEvents pbx_close As PictureBox
     Friend WithEvents tlp_categorias As TableLayoutPanel
     Friend WithEvents pbx_icon As PictureBox
