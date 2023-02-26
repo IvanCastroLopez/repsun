d79ae5996cdea51d9c3ad65db60de5369e82dda1
Leo Aguado <35865347+leoaguado@users.noreply.github.com>
2023-02-27 00:27:31 +0100

    Efecto hover menu terminado

--- repsun/MenuPrincipalForm.Designer.vb
+++ repsun/MenuPrincipalForm.Designer.vb
@@ -26,58 +26,68 @@ Partial Class MenuPrincipalForm
         Me.btn_gestion = New System.Windows.Forms.Button()
         Me.btn_zonaClientes = New System.Windows.Forms.Button()
         Me.btn_surtidores = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_salir = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btn_tpv
         '
-        Me.btn_tpv.Location = New System.Drawing.Point(1021, 533)
+        Me.btn_tpv.BackColor = System.Drawing.Color.White
+        Me.btn_tpv.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 15.85714!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_tpv.Location = New System.Drawing.Point(999, 531)
         Me.btn_tpv.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_tpv.Name = "btn_tpv"
-        Me.btn_tpv.Size = New System.Drawing.Size(189, 66)
+        Me.btn_tpv.Size = New System.Drawing.Size(233, 66)
         Me.btn_tpv.TabIndex = 4
         Me.btn_tpv.Text = "TPV"
-        Me.btn_tpv.UseVisualStyleBackColor = True
+        Me.btn_tpv.UseVisualStyleBackColor = False
         '
         'btn_gestion
         '
-        Me.btn_gestion.Location = New System.Drawing.Point(788, 708)
+        Me.btn_gestion.BackColor = System.Drawing.Color.White
+        Me.btn_gestion.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 15.85714!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_gestion.Location = New System.Drawing.Point(766, 706)
         Me.btn_gestion.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_gestion.Name = "btn_gestion"
-        Me.btn_gestion.Size = New System.Drawing.Size(175, 66)
+        Me.btn_gestion.Size = New System.Drawing.Size(219, 66)
         Me.btn_gestion.TabIndex = 5
         Me.btn_gestion.Text = "GESTIÓN"
-        Me.btn_gestion.UseVisualStyleBackColor = True
+        Me.btn_gestion.UseVisualStyleBackColor = False
         '
         'btn_zonaClientes
         '
-        Me.btn_zonaClientes.Location = New System.Drawing.Point(1311, 609)
+        Me.btn_zonaClientes.BackColor = System.Drawing.Color.White
+        Me.btn_zonaClientes.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 15.85714!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_zonaClientes.Location = New System.Drawing.Point(1289, 607)
         Me.btn_zonaClientes.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_zonaClientes.Name = "btn_zonaClientes"
-        Me.btn_zonaClientes.Size = New System.Drawing.Size(211, 66)
+        Me.btn_zonaClientes.Size = New System.Drawing.Size(255, 66)
         Me.btn_zonaClientes.TabIndex = 6
         Me.btn_zonaClientes.Text = "ZONA CLIENTES"
-        Me.btn_zonaClientes.UseVisualStyleBackColor = True
+        Me.btn_zonaClientes.UseVisualStyleBackColor = False
         '
         'btn_surtidores
         '
-        Me.btn_surtidores.Location = New System.Drawing.Point(1602, 401)
+        Me.btn_surtidores.BackColor = System.Drawing.Color.White
+        Me.btn_surtidores.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 15.85714!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_surtidores.Location = New System.Drawing.Point(1580, 399)
         Me.btn_surtidores.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_surtidores.Name = "btn_surtidores"
-        Me.btn_surtidores.Size = New System.Drawing.Size(260, 73)
+        Me.btn_surtidores.Size = New System.Drawing.Size(304, 73)
         Me.btn_surtidores.TabIndex = 7
         Me.btn_surtidores.Text = "SURTIDORES"
-        Me.btn_surtidores.UseVisualStyleBackColor = True
+        Me.btn_surtidores.UseVisualStyleBackColor = False
         '
-        'Button1
+        'btn_salir
         '
-        Me.Button1.Location = New System.Drawing.Point(1242, 966)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(137, 63)
-        Me.Button1.TabIndex = 8
-        Me.Button1.Text = "SALIR"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btn_salir.BackColor = System.Drawing.Color.White
+        Me.btn_salir.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 15.85714!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_salir.Location = New System.Drawing.Point(1220, 964)
+        Me.btn_salir.Margin = New System.Windows.Forms.Padding(4)
+        Me.btn_salir.Name = "btn_salir"
+        Me.btn_salir.Size = New System.Drawing.Size(181, 63)
+        Me.btn_salir.TabIndex = 8
+        Me.btn_salir.Text = "SALIR"
+        Me.btn_salir.UseVisualStyleBackColor = False
         '
         'MenuPrincipalForm
         '
@@ -86,7 +96,7 @@ Partial Class MenuPrincipalForm
         Me.BackgroundImage = Global.repsun.My.Resources.Resources._0_Default
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(1920, 1080)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btn_salir)
         Me.Controls.Add(Me.btn_tpv)
         Me.Controls.Add(Me.btn_gestion)
         Me.Controls.Add(Me.btn_zonaClientes)
@@ -105,5 +115,5 @@ Partial Class MenuPrincipalForm
     Friend WithEvents btn_gestion As Button
     Friend WithEvents btn_zonaClientes As Button
     Friend WithEvents btn_surtidores As Button
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btn_salir As Button
 End Class
--- repsun/MenuPrincipalForm.vb
+++ repsun/MenuPrincipalForm.vb
@@ -1,30 +1,78 @@
 ﻿Public Class MenuPrincipalForm
+    Public imgDefault, imgCliente, imgTienda, imgGestion, imgSurtidores, imgSalir As Image
+    Private Sub MenuPrincipalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            imgDefault = Image.FromFile("Imagenes\MenuVisual\0-Default.png")
+            Me.BackgroundImage = imgDefault
+            imgCliente = Image.FromFile("Imagenes\MenuVisual\2-Cliente.png")
+            imgTienda = Image.FromFile("Imagenes\MenuVisual\3-Tienda.png")
+            imgGestion = Image.FromFile("Imagenes\MenuVisual\4-Gestion.png")
+            imgSurtidores = Image.FromFile("Imagenes\MenuVisual\5-Surtidores.png")
+            imgSalir = Image.FromFile("Imagenes\MenuVisual\Salir.png")
+        Catch ex As Exception
+            Registros.GrabarError("Hubo un problema al cargar las imagenes", "Error Imagenes")
+        End Try
+    End Sub
 
     Private Sub btn_surtidores_Click(sender As Object, e As EventArgs) Handles btn_surtidores.Click
         SurtidorOnTop.Show()
         Me.Hide()
     End Sub
 
-    Private Sub MenuPrincipalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub btn_gestion_Click_1(sender As Object, e As EventArgs) Handles btn_gestion.Click
+    Private Sub btn_gestion_Click(sender As Object, e As EventArgs) Handles btn_gestion.Click
         GestionForm.Show()
         Me.Hide()
     End Sub
 
-    Private Sub btn_tpv_Click_1(sender As Object, e As EventArgs) Handles btn_tpv.Click
+    Private Sub btn_tpv_Click(sender As Object, e As EventArgs) Handles btn_tpv.Click
         tpvForm.Show()
         Me.Hide()
     End Sub
 
-    Private Sub btn_zonaClientes_Click_1(sender As Object, e As EventArgs) Handles btn_zonaClientes.Click
+    Private Sub btn_zonaClientes_Click(sender As Object, e As EventArgs) Handles btn_zonaClientes.Click
         ClientesOnTop.booleanCrear = True
         ClientesOnTop.ShowDialog()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub btn_salir_Click(sender As Object, e As EventArgs) Handles btn_salir.Click
         End
+    End Sub
+
+
+
+    '** EFECTO HOVER BOTONES **
+
+    Private Sub setDefaultBackground()
+        Me.BackgroundImage = imgDefault
+    End Sub
+    Private Sub btn_gestion_MouseHover(sender As Object, e As EventArgs) Handles btn_gestion.MouseHover
+        Me.BackgroundImage = imgGestion
+    End Sub
+    Private Sub btn_gestion_MouseLeave(sender As Object, e As EventArgs) Handles btn_gestion.MouseLeave
+        setDefaultBackground()
+    End Sub
+    Private Sub btn_salir_MouseHover(sender As Object, e As EventArgs) Handles btn_salir.MouseHover
+        Me.BackgroundImage = imgSalir
+    End Sub
+    Private Sub btn_salir_MouseLeave(sender As Object, e As EventArgs) Handles btn_salir.MouseLeave
+        setDefaultBackground()
+    End Sub
+    Private Sub btn_surtidores_MouseHover(sender As Object, e As EventArgs) Handles btn_surtidores.MouseHover
+        Me.BackgroundImage = imgSurtidores
+    End Sub
+    Private Sub btn_surtidores_MouseLeave(sender As Object, e As EventArgs) Handles btn_surtidores.MouseLeave
+        setDefaultBackground()
+    End Sub
+    Private Sub btn_tpv_MouseHover(sender As Object, e As EventArgs) Handles btn_tpv.MouseHover
+        Me.BackgroundImage = imgTienda
+    End Sub
+    Private Sub btn_tpv_MouseLeave(sender As Object, e As EventArgs) Handles btn_tpv.MouseLeave
+        setDefaultBackground()
+    End Sub
+    Private Sub btn_zonaClientes_MouseHover(sender As Object, e As EventArgs) Handles btn_zonaClientes.MouseHover
+        Me.BackgroundImage = imgCliente
+    End Sub
+    Private Sub btn_zonaClientes_MouseLeave(sender As Object, e As EventArgs) Handles btn_zonaClientes.MouseLeave
+        setDefaultBackground()
     End Sub
 End Class
