ec71c366605f593548a62293b1a876579f8ddbb5
Leo Aguado <35865347+leoaguado@users.noreply.github.com>
2023-02-27 00:04:48 +0100

    Botones menu

--- repsun/MenuPrincipalForm.Designer.vb
+++ repsun/MenuPrincipalForm.Designer.vb
@@ -26,47 +26,58 @@ Partial Class MenuPrincipalForm
         Me.btn_gestion = New System.Windows.Forms.Button()
         Me.btn_zonaClientes = New System.Windows.Forms.Button()
         Me.btn_surtidores = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btn_tpv
         '
-        Me.btn_tpv.Location = New System.Drawing.Point(941, 245)
+        Me.btn_tpv.Location = New System.Drawing.Point(1021, 533)
         Me.btn_tpv.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_tpv.Name = "btn_tpv"
-        Me.btn_tpv.Size = New System.Drawing.Size(297, 272)
+        Me.btn_tpv.Size = New System.Drawing.Size(189, 66)
         Me.btn_tpv.TabIndex = 4
         Me.btn_tpv.Text = "TPV"
         Me.btn_tpv.UseVisualStyleBackColor = True
         '
         'btn_gestion
         '
-        Me.btn_gestion.Location = New System.Drawing.Point(706, 110)
+        Me.btn_gestion.Location = New System.Drawing.Point(788, 708)
         Me.btn_gestion.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_gestion.Name = "btn_gestion"
-        Me.btn_gestion.Size = New System.Drawing.Size(210, 103)
+        Me.btn_gestion.Size = New System.Drawing.Size(175, 66)
         Me.btn_gestion.TabIndex = 5
         Me.btn_gestion.Text = "GESTIÓN"
         Me.btn_gestion.UseVisualStyleBackColor = True
         '
         'btn_zonaClientes
         '
-        Me.btn_zonaClientes.Location = New System.Drawing.Point(1321, 472)
+        Me.btn_zonaClientes.Location = New System.Drawing.Point(1311, 609)
         Me.btn_zonaClientes.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_zonaClientes.Name = "btn_zonaClientes"
-        Me.btn_zonaClientes.Size = New System.Drawing.Size(221, 117)
+        Me.btn_zonaClientes.Size = New System.Drawing.Size(211, 66)
         Me.btn_zonaClientes.TabIndex = 6
         Me.btn_zonaClientes.Text = "ZONA CLIENTES"
         Me.btn_zonaClientes.UseVisualStyleBackColor = True
         '
         'btn_surtidores
         '
-        Me.btn_surtidores.Location = New System.Drawing.Point(1265, 51)
+        Me.btn_surtidores.Location = New System.Drawing.Point(1602, 401)
         Me.btn_surtidores.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_surtidores.Name = "btn_surtidores"
-        Me.btn_surtidores.Size = New System.Drawing.Size(642, 220)
+        Me.btn_surtidores.Size = New System.Drawing.Size(260, 73)
         Me.btn_surtidores.TabIndex = 7
         Me.btn_surtidores.Text = "SURTIDORES"
         Me.btn_surtidores.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1242, 966)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(137, 63)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "SALIR"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'MenuPrincipalForm
         '
@@ -75,6 +86,7 @@ Partial Class MenuPrincipalForm
         Me.BackgroundImage = Global.repsun.My.Resources.Resources._0_Default
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(1920, 1080)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btn_tpv)
         Me.Controls.Add(Me.btn_gestion)
         Me.Controls.Add(Me.btn_zonaClientes)
@@ -93,4 +105,5 @@ Partial Class MenuPrincipalForm
     Friend WithEvents btn_gestion As Button
     Friend WithEvents btn_zonaClientes As Button
     Friend WithEvents btn_surtidores As Button
+    Friend WithEvents Button1 As Button
 End Class
--- repsun/MenuPrincipalForm.vb
+++ repsun/MenuPrincipalForm.vb
@@ -23,4 +23,8 @@
         ClientesOnTop.booleanCrear = True
         ClientesOnTop.ShowDialog()
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
 End Class
