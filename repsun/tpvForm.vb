872ff513f3f9401b4da78585753e9564b6a164be
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-02-28 19:31:06 +0100

    no funciona la tpv

--- repsun/tpvForm.vb
+++ repsun/tpvForm.vb
@@ -1,4 +1,6 @@
-﻿Imports System.Data.OleDb
+﻿Imports System.Collections.ObjectModel
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
 
 Public Class tpvForm
     ' Aquí se establece la conexión a la base de datos mediante el proveedor Microsoft.ACE.OLEDB.12.0 y se especifica la ubicación de la base de datos.
@@ -6,14 +8,13 @@ Public Class tpvForm
 
     'Aquí se crea un adaptador de datos OleDbDataAdapter para seleccionar todos los datos de la tabla "Producto" de la base de datos.
     Public adaptador_tienda As New OleDbDataAdapter("Select * from Producto", conexion)
+    Dim adaptador_cesta As New OleDbDataAdapter("SELECT * FROM CestaCompra", conexion)
 
     'Aquí se crea un objeto DataSet llamado "gestion_dataset".
     Public gestion_dataset As New DataSet
 
     ' Creamos un objeto DataTable para almacenar los productos del carrito.
     Public carrito As New DataTable
-
-    Private productos As DataTable
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CrearTLP("Comida")
@@ -108,7 +109,7 @@ Public Class tpvForm
                     btn.Text = nombreItem & vbCrLf & FormatCurrency(precioItem)
                     btn.Dock = DockStyle.Fill
                     AddHandler btn.Click, Sub(sender As Object, e As EventArgs)
-                                              AgregarProducto(codigoItem)
+                                              agregarProducto(codigoItem)
                                           End Sub
                     tlp.Controls.Add(btn, col, row)
                 Next
@@ -126,52 +127,75 @@ Public Class tpvForm
 
 
     ' ** CARRITO DE COMPRA **
+    Private Function ObtenerProductoPorCodigo(codigo As Integer) As DataRow
+        Dim producto As DataRow = Nothing
+
+        ' Crear un comando para obtener el producto por código
+        Dim cmd As New OleDbCommand("SELECT * FROM Producto WHERE cod_producto = @codigo", conexion)
+        cmd.Parameters.AddWithValue("@codigo", codigo)
+
+        Try
+            ' Abrir la conexión y ejecutar el comando
+            conexion.Open()
+            Dim adaptador As New OleDbDataAdapter(cmd)
+            Dim tablaProductos As New DataTable()
+            adaptador.Fill(tablaProductos)
+
+            ' Obtener la primera fila de la tabla
+            If tablaProductos.Rows.Count > 0 Then
+                producto = tablaProductos.Rows(0)
+            End If
+        Catch ex As Exception
+            ' Mostrar un mensaje de error si ocurre una excepción
+            MessageBox.Show("Error al obtener el producto: " & ex.Message)
+        Finally
+            ' Cerrar la conexión
+            conexion.Close()
+        End Try
+
+        Return producto
+    End Function
 
 
-    Private Sub AgregarProducto(codigo As Integer)
-        ' Creamos el objeto DataTable productos
-        Dim productos As DataTable = gestion_dataset.Tables("Producto")
-        ' Establecemos la clave principal en la columna "cod_producto"
-        productos.PrimaryKey = New DataColumn() {productos.Columns("cod_producto")}
 
-        Dim cesta As DataTable = gestion_dataset.Tables("CestaCompra")
+    Private Sub agregarProducto(codigo As Integer)
+        ' Buscar el producto en la base de datos
+        Dim producto As DataRow = ObtenerProductoPorCodigo(codigo)
 
-        ' Buscamos el producto en la tabla
-        Dim productoFila As DataRow = productos.Rows.Find(codigo)
-
-        If productoFila IsNot Nothing Then
-            ' Si el producto ya existe, incrementamos la cantidad y actualizamos el total
-            Dim cantidad As Integer = 0
-            Dim precio As Decimal = 0
-
+        If producto IsNot Nothing Then
+            ' Buscar si el producto ya está en la cesta
+            Dim cesta As DataTable = gestion_dataset.Tables("CestaCompra")
             Dim cestaFila As DataRow = cesta.Rows.Find(codigo)
 
             If cestaFila IsNot Nothing Then
-                cantidad = cestaFila("cantidad")
-                precio = cestaFila("precio_por_litro")
-                cestaFila("cantidad") = cantidad + 1
-                cestaFila("total") = (cantidad + 1) * precio
+                ' Si el producto ya está en la cesta, actualizar la cantidad, el precio y el total
+                cestaFila("cantidad") = CInt(cestaFila("cantidad")) + 1
+                cestaFila("total") = CDec(cestaFila("cantidad")) * CDec(cestaFila("precio_por_litro"))
             Else
-                cantidad = 1
-                precio = productoFila("precio")
-                Dim newRow As DataRow = cesta.NewRow()
-                newRow("cod_producto") = codigo
-                newRow("cantidad") = 1
-                newRow("precio_por_litro") = precio
-                newRow("total") = precio
-                cesta.Rows.Add(newRow)
+                ' Si el producto no está en la cesta, añadir una nueva fila
+                Dim nuevaFila As DataRow = cesta.NewRow()
+                nuevaFila("cod_cesta") = codigo
+                nuevaFila("cod_producto") = codigo
+                nuevaFila("cantidad") = 1
+                nuevaFila("total") = producto("precio")
+                nuevaFila("precio_por_litro") = producto("precio")
+                cesta.Rows.Add(nuevaFila)
             End If
 
-            ' Buscamos la fila correspondiente en el DataGridView y actualizamos la cantidad y el total
-            For Each row As DataGridViewRow In dgv_carrito.Rows
-                If row.Cells("codigo").Value = codigo Then
-                    row.Cells("cantidad").Value = cantidad
-                    row.Cells("total").Value = cantidad * precio
-                    Exit Sub
+            ' Actualizar el DataGridView con los datos de la cesta
+            dgv_carrito.Rows.Clear()
+
+            For Each filaCesta As DataRow In cesta.Rows
+                Dim filaProducto As DataRow = gestion_dataset.Tables("Producto").Rows.Find(filaCesta("cod_producto"))
+
+                If filaProducto IsNot Nothing Then
+                    dgv_carrito.Rows.Add({filaCesta("cantidad"), filaCesta("cod_producto"), filaProducto("nombre"), filaCesta("precio_por_litro")})
                 End If
             Next
         Else
-            MessageBox.Show("El código del producto no existe")
+            MessageBox.Show("El código del producto no existe.")
         End If
     End Sub
+
+
 End Class
